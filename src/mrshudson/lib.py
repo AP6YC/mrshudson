d5aa88a1bf05992e39b1dd818baec0ab03ddcd69
Sasha Petrenko <sap625@umsystem.edu>
2022-09-23 12:23:20 -0500

    Ignore IDE files, clean up linting

--- src/mrshudson/lib.py
+++ src/mrshudson/lib.py
@@ -32,17 +32,35 @@ from . import constants
 # FUNCTIONS
 # --------------------------------------------------------------------------- #
 
+
+def get_methods(my_class):
+    """
+    Gets the non-wrangled methods and members of a class.
+    """
+    method_list = [method for method in dir(my_class) if method.startswith('__') is False]
+    return method_list
+
+
+def print_methods(my_class):
+    """
+    Prints the non-wrangled methods and members of a class.
+    """
+    print(get_methods(my_class))
+
+
 def get_project_root() -> Path:
     """Returns the top project directory.
     """
 
     return Path(__file__).resolve().parent.parent.parent
 
+
 def projectdir(*args) -> Path:
     """Returns the DrWatson-like project directory.
     """
 
     return get_project_root().joinpath(*args)
+
 
 def load_layout(layout_file):
     """Loads and returns the provided YAML project layout file.
@@ -51,13 +69,15 @@ def load_layout(layout_file):
     try:
         with open(layout_file, 'r') as file:
             layout = yaml.safe_load(file)
-    except:
+    except Exception:
         logging.warning("Could not open layout file, loading default layout.")
         layout = constants.DEFAULT_LAYOUT
 
     return layout
 
+
 LAYOUT = load_layout(projectdir(constants.DEFAULT_LAYOUT_FILE))
+
 
 def plotsdir(*args) -> Path:
     """Returns the DrWatson-like plots directory.
@@ -65,11 +85,13 @@ def plotsdir(*args) -> Path:
 
     return projectdir().joinpath(LAYOUT["plots_dir"], *args)
 
+
 def papersdir(*args) -> Path:
     """Returns the DrWatson-like papers directory.
     """
 
     return projectdir().joinpath(LAYOUT["papers_dir"], *args)
+
 
 def srcdir(*args) -> Path:
     """Returns the DrWatson-like source directory.
@@ -77,11 +99,13 @@ def srcdir(*args) -> Path:
 
     return projectdir().joinpath(LAYOUT["src_dir"], *args)
 
+
 def scriptsdir(*args) -> Path:
     """Returns the DrWatson-like scripts directory.
     """
 
     return projectdir().joinpath(LAYOUT["scripts_dir"], *args)
+
 
 def optsdir(*args) -> Path:
     """Returns the DrWatson-like options directory.
@@ -89,11 +113,13 @@ def optsdir(*args) -> Path:
 
     return projectdir().joinpath(LAYOUT["opts_dir"], *args)
 
+
 def modelsdir(*args) -> Path:
     """Returns the DrWatson-like models directory.
     """
 
     return projectdir().joinpath(LAYOUT["models_dir"], *args)
+
 
 def datadir(*args) -> Path:
     """Returns the DrWatson-like data directory.
@@ -101,11 +127,13 @@ def datadir(*args) -> Path:
 
     return projectdir().joinpath(LAYOUT["data_dir"], *args)
 
+
 def datadir_raw(*args) -> Path:
     """Returns the DrWatson-like raw data directory.
     """
 
     return datadir(LAYOUT["data_dir_raw"], *args)
+
 
 def datadir_pro(*args) -> Path:
     """Returns the DrWatson-like data directory.
@@ -113,17 +141,20 @@ def datadir_pro(*args) -> Path:
 
     return datadir(LAYOUT["data_dir_pro"], *args)
 
+
 def datadir_sims(*args) -> Path:
     """Returns the DrWatson-like data directory.
     """
 
     return datadir(LAYOUT["data_dir_sims"], *args)
 
+
 def dropboxdir(*args) -> Path:
     """Returns the DrWatson-like dropbox directory.
     """
 
     return projectdir().joinpath(LAYOUT["dropbox_dir"], *args)
+
 
 def localize(dest_name, *args) -> tuple:
     """Takes DrWatson directory functions and returns subdirectory functions with `dest_name`.
@@ -134,6 +165,7 @@ def localize(dest_name, *args) -> tuple:
     # for arg in args:
     #     funcs.append(lambda *local_args, my_arg=arg: my_arg(dest_name, *local_args))
     # return funcs
+
 
 def localize_and_make(dest_name, *args) -> tuple:
     """Localize the directory functions to the `dest_name` and make the directories at the same time.
