4e8812fab5edb7d37cda2409fac5376076d54cdf
Sasha Petrenko <sap625@umsystem.edu>
2024-02-05 18:25:36 -0600

    Rework types, temporary monofile, rework docstrings

--- src/mrshudson/__init__.py
+++ src/mrshudson/__init__.py
@@ -2,17 +2,6 @@
 mrshudson - a Python scientific project assistant package.
 """
 
-__version__ = "0.1.0-alpha.1"
-
-# from .lib import *
-
-
-"""
-    lib.py
-
-mrshudson's utility functions.
-"""
-
 # -----------------------------------------------------------------------------
 # STDLIB IMPORTS
 # -----------------------------------------------------------------------------
@@ -22,25 +11,56 @@ from pathlib import Path
 import logging as lg
 import warnings
 
-from typing import (
+# -----------------------------------------------------------------------------
+# EXTERNAL DEPENDENCY IMPORTS
+# -----------------------------------------------------------------------------
+
+from typing_extensions import (
     List,
     Callable,
     TypeAlias,
+    Dict,
 )
-
-# -----------------------------------------------------------------------------
-# EXTERNAL DEPENDENCIES
-# -----------------------------------------------------------------------------
-
-# import yaml
-# from multimethod import multimethod
 
 # -----------------------------------------------------------------------------
 # LOCAL IMPORTS
 # -----------------------------------------------------------------------------
 
-# import src.python.constants as cs
-from . import constants
+# from . import constants
+
+# -----------------------------------------------------------------------------
+# PACKAGE ATTRIBUTES
+# -----------------------------------------------------------------------------
+
+__version__ = "0.1.0-alpha.1"
+
+# -----------------------------------------------------------------------------
+# TYPE ALIASES
+# -----------------------------------------------------------------------------
+
+TypeLayout: TypeAlias = Dict[str, str]
+"""TypeAlias: A type alias for the layout of a project.
+"""     # pylint: disable=W0105
+
+TypeDirFuncs: TypeAlias = List[Callable]
+"""TypeAlias: A type alias for the list of directory functions.
+"""
+
+DEFAULT_LAYOUT: TypeLayout = {
+    "plots_dir": "plots",
+    "papers_dir": "papers",
+    "scripts_dir": "scripts",
+    "src_dir": "src",
+    "opts_dir": "opts",
+    "data_dir": "data",
+    "models_dir": "models",
+    "notebooks_dir": "notebooks",
+    "data_dir_raw": "data/exp_raw",
+    "data_dir_pro": "data/exp_pro",
+    "data_dir_sims": "data/sims",
+}
+"""TypeLayout: The default :mod:`mrshudson` project layout.
+"""     # pylint: disable=W0105
 
 # -----------------------------------------------------------------------------
 # CLASSES
@@ -63,6 +83,12 @@ class ProjectState():
         If low, the default the :func:`~default_projectdir` function is used to get the current :attr:`~ProjectState.projectdir`.
         """
 
+        self.layout: TypeLayout = DEFAULT_LAYOUT
+        """The layout of the project directory structure.
+
+        This is used both for instantiating a project and for pointing to the correct locations when using the directory functions.
+        """
+
         return
 
     def _default_projectdir(self):
@@ -80,12 +106,15 @@ class ProjectState():
             new_projectdir (Path): the new `pathlib.Path` that points to new :attr:`~ProjectState.projectdir` to use.
         """
 
+        # Set the project directory to the provided path
         self.projectdir = new_projectdir
+
+        # Tell the module that the directory was manually set
         self.projectdir_was_set = True
 
         return
 
-    def get_projectdir(self):
+    def _get_projectdir(self):
         """Returns the current top-level project directory.
 
         If the value is manually set, it returns :attr:`~ProjectState.projectdir`.
@@ -97,20 +126,83 @@ class ProjectState():
         else:
             return self._default_projectdir()
 
+
 # -----------------------------------------------------------------------------
 # GLOBAL CONSTANTS
 # -----------------------------------------------------------------------------
 
 
-globalprojectstate = ProjectState()
-"""The default project state that is used when mrshudson directory functions.
+globalprojectstate: ProjectState = ProjectState()
+"""ProjectState: The default project state that is used when mrshudson directory functions.
 """
+
+
+# DEFAULT_LAYOUT_FILE: str = "layout.yml"
+# """The location of the default layout file
+# """     # pylint: disable=W0105
+
+
+# LAYOUT = constants.DEFAULT_LAYOUT
+# LAYOUT = DEFAULT_LAYOUT
+
+_ARG_ARGS = """
+    *args: the subdirectories/files to join to the path.
+"""
+
+_ARG_PROJECT_STATE = """
+    project_state (ProjectState, optional): The stateful representation of the project structure to use. Defaults to globalprojectstate
+"""
+
+_ARG_ARGS_PROJECT_STATE = f"""
+Args:
+    {_ARG_ARGS}
+    {_ARG_PROJECT_STATE}
+"""
+
+
+# TypeLayout: TypeAlias = Dict[str, Path]
+# """A type alias for the layout of a project.
+# """     # pylint: disable=W0105
+
+# DEFAULT_LAYOUT: TypeLayout = {
+#     "plots_dir": Path("plots",
+#     "papers_dir": Path("papers"),
+#     "scripts_dir": Path("scripts"),
+#     "src_dir": Path("src"),
+#     "opts_dir": Path("opts"),
+#     "data_dir": Path("data"),
+#     "models_dir": Path("models"),
+#     "notebooks_dir": Path("notebooks"),
+#     "data_dir_raw": Path("data", "exp_raw"),
+#     "data_dir_pro": Path("data", "exp_pro"),
+#     "data_dir_sims": Path("data", "sims"),
+# }
+# """The default mrshudson project layout.
+# """     # pylint: disable=W0105
+
+# DEFAULT_TEMPLATE = [
+#   "_research",
+#   "src",
+#   "scripts",
+#   "plots",
+#   "notebooks",
+#   "papers",
+#   "data" => ["sims", "exp_raw", "exp_pro"],
+# ]
+
 
 # -----------------------------------------------------------------------------
 # FUNCTIONS
 # -----------------------------------------------------------------------------
 
+def _docstring_parameter(*sub):
+    def dec(obj):
+        obj.__doc__ = obj.__doc__.format(*sub)
+        return obj
+    return dec
 
+
+@_docstring_parameter(_ARG_PROJECT_STATE)
 def set_projectdir(
     project_name: str,
     project_state: ProjectState = globalprojectstate,
@@ -123,6 +215,7 @@ def set_projectdir(
 
     Args:
         project_name (str): the name of the directory that is at or above of the current working directory to set as the value that is returned when calling :func:`~projectdir`.
+        {0}
     """
 
     # Initiate the local directory for searching up
@@ -144,157 +237,171 @@ def set_projectdir(
     return
 
 
-def get_project_root(project_state: ProjectState = globalprojectstate) -> Path:
-    """Returns the top project directory from the project state.
-    """
-
-    return project_state.get_projectdir()
-
-
-def docstring_parameter(*sub):
-    def dec(obj):
-        obj.__doc__ = obj.__doc__.format(*sub)
-        return obj
-    return dec
+# def _get_project_root(project_state: ProjectState = globalprojectstate) -> Path:
+#     """Returns the top project directory from the project state.
+#     """
+#     return project_state._get_projectdir()
 
 
-ARGS_ARG = """
-
-Args:
-    *args: the subdirectories/files to join to the path.
-"""
-
-
-@docstring_parameter(ARGS_ARG)
-def projectdir(*args) -> Path:
+@_docstring_parameter(_ARG_ARGS_PROJECT_STATE)
+def projectdir(
+    *args,
+    project_state: ProjectState = globalprojectstate,
+) -> Path:
     """Returns the top project directory with optional added path parts.
     {0}
     """
 
-    return get_project_root().joinpath(*args)
+    # return _get_project_root(project_state).joinpath(*args)
+    return project_state._get_projectdir().joinpath(*args)
 
 
-LAYOUT = constants.DEFAULT_LAYOUT
-
-
-@docstring_parameter(ARGS_ARG)
-def plotsdir(*args) -> Path:
+@_docstring_parameter(_ARG_ARGS_PROJECT_STATE)
+def plotsdir(
+    *args,
+    project_state: ProjectState = globalprojectstate,
+) -> Path:
     """Returns the DrWatson-like plots directory.
     {0}
     """
 
-    return projectdir(LAYOUT["plots_dir"], *args)
+    return projectdir(project_state.layout["plots_dir"], *args)
 
 
-@docstring_parameter(ARGS_ARG)
-def papersdir(*args) -> Path:
+@_docstring_parameter(_ARG_ARGS_PROJECT_STATE)
+def papersdir(
+    *args,
+    project_state: ProjectState = globalprojectstate,
+) -> Path:
     """Returns the DrWatson-like papers directory.
     {0}
     """
 
-    return projectdir(LAYOUT["papers_dir"], *args)
+    return projectdir(project_state.layout["papers_dir"], *args)
 
 
-@docstring_parameter(ARGS_ARG)
-def srcdir(*args) -> Path:
+@_docstring_parameter(_ARG_ARGS_PROJECT_STATE)
+def srcdir(
+    *args,
+    project_state: ProjectState = globalprojectstate,
+) -> Path:
     """Returns the DrWatson-like source directory.
     {0}
     """
 
-    return projectdir(LAYOUT["src_dir"], *args)
+    return projectdir(project_state.layout["src_dir"], *args)
 
 
-@docstring_parameter(ARGS_ARG)
-def scriptsdir(*args) -> Path:
+@_docstring_parameter(_ARG_ARGS_PROJECT_STATE)
+def scriptsdir(
+    *args,
+    project_state: ProjectState = globalprojectstate,
+) -> Path:
     """Returns the DrWatson-like scripts directory.
     {0}
     """
 
-    return projectdir(LAYOUT["scripts_dir"], *args)
+    return projectdir(project_state.layout["scripts_dir"], *args)
 
 
-@docstring_parameter(ARGS_ARG)
-def optsdir(*args) -> Path:
+@_docstring_parameter(_ARG_ARGS_PROJECT_STATE)
+def optsdir(
+    *args,
+    project_state: ProjectState = globalprojectstate,
+) -> Path:
     """Returns the DrWatson-like options directory.
     {0}
     """
 
-    return projectdir(LAYOUT["opts_dir"], *args)
+    return projectdir(project_state.layout["opts_dir"], *args)
 
 
-@docstring_parameter(ARGS_ARG)
-def modelsdir(*args) -> Path:
+@_docstring_parameter(_ARG_ARGS_PROJECT_STATE)
+def modelsdir(
+    *args,
+    project_state: ProjectState = globalprojectstate,
+) -> Path:
     """Returns the DrWatson-like models directory.
     {0}
     """
 
-    return projectdir(LAYOUT["models_dir"], *args)
+    return projectdir(project_state.layout["models_dir"], *args)
 
 
-@docstring_parameter(ARGS_ARG)
-def notebooksdir(*args) -> Path:
+@_docstring_parameter(_ARG_ARGS_PROJECT_STATE)
+def notebooksdir(
+    *args,
+    project_state: ProjectState = globalprojectstate,
+) -> Path:
     """Returns the DrWatson-like notebooks directory.
     {0}
     """
 
-    return projectdir(LAYOUT["notebooks_dir"], *args)
+    return projectdir(project_state.layout["notebooks_dir"], *args)
 
 
-@docstring_parameter(ARGS_ARG)
-def datadir(*args) -> Path:
+@_docstring_parameter(_ARG_ARGS_PROJECT_STATE)
+def datadir(
+    *args,
+    project_state: ProjectState = globalprojectstate,
+) -> Path:
     """Returns the DrWatson-like data directory.
     {0}
     """
 
-    return projectdir(LAYOUT["data_dir"], *args)
+    return projectdir(project_state.layout["data_dir"], *args)
 
 
-@docstring_parameter(ARGS_ARG)
-def datadir_raw(*args) -> Path:
+@_docstring_parameter(_ARG_ARGS_PROJECT_STATE)
+def datadir_raw(
+    *args,
+    project_state: ProjectState = globalprojectstate,
+) -> Path:
     """Returns the DrWatson-like raw data directory.
     {0}
     """
 
-    return projectdir(LAYOUT["data_dir_raw"], *args)
+    return projectdir(project_state.layout["data_dir_raw"], *args)
 
 
-@docstring_parameter(ARGS_ARG)
-def datadir_pro(*args) -> Path:
+@_docstring_parameter(_ARG_ARGS_PROJECT_STATE)
+def datadir_pro(
+    *args,
+    project_state: ProjectState = globalprojectstate,
+) -> Path:
     """Returns the DrWatson-like processed data directory.
     {0}
     """
 
-    return projectdir(LAYOUT["data_dir_pro"], *args)
+    return projectdir(project_state.layout["data_dir_pro"], *args)
 
 
-@docstring_parameter(ARGS_ARG)
-def datadir_sims(*args) -> Path:
+@_docstring_parameter(_ARG_ARGS_PROJECT_STATE)
+def datadir_sims(
+    *args,
+    project_state: ProjectState = globalprojectstate,
+) -> Path:
     """Returns the DrWatson-like simulations data directory.
     {0}
     """
 
-    return projectdir(LAYOUT["data_dir_sims"], *args)
+    return projectdir(project_state.layout["data_dir_sims"], *args)
 
 
 def initialize_project(
-    layout=constants.DEFAULT_LAYOUT
+    # layout=DEFAULT_LAYOUT
+    project_state: ProjectState = globalprojectstate,
 ):
     """Initializes a new mrshudon project from the provided project layout dictionary.
-
-    Args:
-        layout=constants.DEFAULT_LAYOUT
     """
 
     # For each
-    for key, local_path in layout.items():
+    for key, local_path in project_state.layout.items():
         lg.info(f"Making directory: {key} at {local_path}")
         # projectdir(local_path).mkdir(parents=True, exist_ok=True)
         projectdir(Path(local_path)).mkdir(parents=True, exist_ok=True)
 
     return
-
-
-TypeDirFuncs: TypeAlias = List[Callable]
 
 
 DIRFUNCS: TypeDirFuncs = [
@@ -312,3 +419,21 @@ DIRFUNCS: TypeDirFuncs = [
 ]
 """A list of the functions in the mrshudson module that point to project directories.
 """     # pylint: disable=W0105
+
+
+__all__ = [
+    "ProjectState",
+    "set_projectdir",
+    "initialize_project",
+    "projectdir",
+    "plotsdir",
+    "papersdir",
+    "srcdir",
+    "scriptsdir",
+    "optsdir",
+    "modelsdir",
+    "datadir",
+    "datadir_raw",
+    "datadir_pro",
+    "datadir_sims",
+]
